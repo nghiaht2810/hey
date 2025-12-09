d2b9383a294597d82f5adad6e062185922f3c6c6
nghiaht2810 <33nghia2002@gmail.com>
2025-12-10 04:05:35 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import path from "path";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [
@@ -9,8 +10,11 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true, // Đã thêm theo đề xuất trước đó
+        v3_singleFetch: true,
       },
     }),
+    netlifyPlugin(),
   ],
   resolve: {
     alias: {
